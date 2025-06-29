447f053e071bf8974c1d1aaedda42383aea4e8fe
abeer415 <abeersolaiman72@gmail.com>
2025-06-29 20:47:12 +0300

    Register , login ,logout for provider and user (provider middleware isProvider)

--- app/Http/Controllers/ProviderController.php
+++ app/Http/Controllers/ProviderController.php
@@ -8,7 +8,22 @@ class ProviderController extends Controller
 {
    
      
+      public function providerLogout(Request $request){
+
+$request->user()->currentAccessToken()->delete();
+return response()->json([
+            
+  'message'=>' provider logout Successfully',
+ 
+  
+  ] 
+           );
      
+        }
+      
+      
+
+
       
       
 
--- routes/api.php
+++ routes/api.php
@@ -3,6 +3,7 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\QrController;
+use App\Http\Controllers\ProviderController;
 use App\Http\Controllers\UserController;
 Route::get('/user', function (Request $request) {
     return $request->user();
@@ -15,3 +16,6 @@ Route::post('/qr-generator', [QrController::class, 'generatePdfApi']);
 Route::post('register',[UserController::class,'register']);
 Route::post('login',[UserController::class,'login']);
 Route::post('logout',[UserController::class,'logout'])->middleware('auth:sanctum');
+Route::post('provider/logout', [ProviderController::class, 'providerLogout'])
+    ->middleware(['auth:sanctum', 'isProvider']);
+
